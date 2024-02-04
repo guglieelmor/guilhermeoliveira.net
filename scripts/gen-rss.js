a69cedc797c49be10aae4a8b6f339b6e0ec1495e
Gulliver <guglieelmor@gmail.com>
2024-02-03 21:45:09 -0300

    settings feed

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -10,32 +10,17 @@ async function generate() {
     feed_url: 'https://guilhermeoliveira.net/feed.xml'
   })
 
-  const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
-  const allPosts = []
+  const posts = await fs.readdir(path.join(__dirname, '..', 'pages'))
   await Promise.all(
     posts.map(async (name) => {
       if (name.startsWith('index.')) return
 
-      const content = await fs.readFile(
-        path.join(__dirname, '..', 'pages', 'posts', name)
+      await fs.readFile(
+        path.join(__dirname, '..', 'pages', name)
       )
-      const frontmatter = matter(content)
 
-      allPosts.push({
-        title: frontmatter.data.title,
-        url: '/posts/' + name.replace(/\.mdx?/, ''),
-        date: frontmatter.data.date,
-        description: frontmatter.data.description,
-        categories: frontmatter.data.tag.split(', '),
-        author: frontmatter.data.author
-      })
     })
-  )
-
-  allPosts.sort((a, b) => new Date(b.date) - new Date(a.date))
-  allPosts.forEach((post) => {
-      feed.item(post)
-  })
+  ) 
   await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
 }
 
