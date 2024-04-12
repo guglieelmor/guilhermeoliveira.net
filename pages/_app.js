ba43dd1976ffc61c6bd3192b9657ae97e6d4d6c9
Gulliver <guglieelmor@gmail.com>
2024-04-12 01:00:35 -0300

    google

--- pages/_app.js
+++ pages/_app.js
@@ -3,37 +3,12 @@ import Head from 'next/head'
 import { Analytics } from '@vercel/analytics/react';
 
 import '../styles/main.css'
+import { GoogleTagManager } from '@next/third-parties/google';
 
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          title="RSS"
-          href="/feed.xml"
-        />
-        <link
-          rel="preload"
-          href="/fonts/Inter-roman.latin.var.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        dangerouslySetInnerHTML={{
-          __html: `
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-JTP5KZ6L2C"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          
-            gtag('config', 'G-JTP5KZ6L2C');
-          </script>
-          `,
-        }}
-      </Head>
+      <GoogleTagManager gtmId="G-JTP5KZ6L2C" />
       <Component {...pageProps} />
       <Analytics />
     </>
